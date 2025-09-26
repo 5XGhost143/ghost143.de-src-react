13609143080b95cd67163e6e458a2caab2a9c108
5XGhost143 <james.kinzer@t-online.de>
2025-09-26 22:38:53 +0200

    tell me why

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ghost143.de-src-react/',  // 👈 Add this line
+  base: '/',
 })
